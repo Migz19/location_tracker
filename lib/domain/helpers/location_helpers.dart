0615630fb575e37d9bf84de2e25b509420f80527
Ahmed Magdy <ahmedmagdy373@gmail.com>
2024-05-26 22:00:48 +0300

    [Update] The maps is displayed correctly

--- lib/domain/helpers/location_helpers.dart
+++ lib/domain/helpers/location_helpers.dart
@@ -2,11 +2,11 @@ import 'package:geolocator/geolocator.dart';
 class LocationHelper{
   bool isServiceEnabled=false;
   Future<Position>getCurrentLocation()async {
-    print("4203903249023");
+
     isServiceEnabled = await Geolocator.isLocationServiceEnabled();
     LocationPermission permission = await Geolocator.checkPermission();
     if (!isServiceEnabled) {
-      print("4203903249023");
+
       await Geolocator.requestPermission();
     }
     if (permission == LocationPermission.denied) {
--- lib/ui/maps_screen.dart
+++ lib/ui/maps_screen.dart
@@ -13,74 +13,98 @@ class MapsScreen extends StatefulWidget {
 }
 
 class _MapsScreenState extends State<MapsScreen> {
-  static Position? _currentLocation;
+  Position? _currentLocation;
+  Completer<GoogleMapController> _mapsController = Completer<GoogleMapController>();
+
 
   @override
   void initState() {
     super.initState();
     getCurrentLocation();
-
   }
-
-  Completer<GoogleMapController> _mapsController = Completer<GoogleMapController>();
 
   Future<void> getCurrentLocation() async {
-    _currentLocation = await LocationHelper().getCurrentLocation().whenComplete(() { setState(() {
-
-    });});
+    _currentLocation = await LocationHelper().getCurrentLocation();
+    if (mounted) {
+      setState(() {});
+    }
   }
 
-  static CameraPosition? _cameraPosition;
-
-  Widget buildMap() {
-    print("3249892348 ${_currentLocation!.longitude}");
-    _cameraPosition = CameraPosition(
-        target: LatLng(_currentLocation!.latitude, _currentLocation!.longitude),
-        bearing: 0,
-        tilt: 0,
-        zoom: 17);
-    return GoogleMap(
-      mapType: MapType.normal,
-      myLocationEnabled: true,
-      initialCameraPosition: _cameraPosition!,
-      compassEnabled: true,
-      zoomControlsEnabled: false,
-      myLocationButtonEnabled: true,
-      onMapCreated: (GoogleMapController controller) {
-        _mapsController.complete(controller);
-      },
+  CameraPosition get _initialCameraPosition {
+    print("312784237842${_currentLocation!.latitude}");
+    return _currentLocation != null
+        ? CameraPosition(
+      target: LatLng(_currentLocation!.latitude, _currentLocation!.longitude),
+      zoom: 17,
+    )
+        : const CameraPosition(
+      target: LatLng(31.324329, 31.2391293),
+      zoom: 1,
     );
   }
+
+  Widget buildMap() {
+    if (_currentLocation == null) {
+      return const Center(
+        child: CircularProgressIndicator(
+          color: Colors.amberAccent,
+        ),
+      );
+    } else {
+      return GoogleMap(
+        mapType: MapType.normal,
+        // check the parameters
+        myLocationEnabled: true,
+        initialCameraPosition: _initialCameraPosition,
+
+
+        compassEnabled: true,
+        zoomControlsEnabled: false,
+        myLocationButtonEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          if (!_mapsController.isCompleted) {
+            _mapsController.complete(controller);
+          }
+
+        },
+      );
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          _currentLocation == null
-              ? const Center(
-                  child: CircularProgressIndicator(
-                    color: Colors.amberAccent,
-                  ),
-                )
-              : buildMap(),
+          buildMap(),
         ],
       ),
-      floatingActionButton: Container(
-        margin: EdgeInsets.only(bottom: 5.0, top: 5.0, left: 3.0, right: 2.0),
-        child: FloatingActionButton(
-          onPressed: navigateToCurrentLocation,
-          child: Icon(
-            Icons.place,
-            color: Colors.red,
-          ),
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: navigateToCurrentLocation,
+        child: const Icon(
+          Icons.place,
+          color: Colors.red,
         ),
-      ),
+      ),*/
     );
   }
 
   Future<void> navigateToCurrentLocation() async {
-    final GoogleMapController controller = await _mapsController.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_cameraPosition!));
+    if (_currentLocation != null ) {
+      final GoogleMapController controller = await _mapsController.future;
+      if (mounted) {
+        controller.animateCamera(
+          CameraUpdate.newCameraPosition(_initialCameraPosition),
+        );
+      }
+    }
   }
+  Future<void> _goToMyCurrentLocation() async {
+    final GoogleMapController controller = await _mapsController.future;
+    controller.animateCamera(
+        CameraUpdate.newCameraPosition(_initialCameraPosition));
+  }
+
+
 }
